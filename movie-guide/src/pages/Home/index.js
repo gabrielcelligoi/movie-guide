5e20e196838bca6d7a749f07e93fbeb2f944228d
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-17 16:02:01 -0300

    load movies from api on hpme

--- movie-guide/src/pages/Home/index.js
+++ movie-guide/src/pages/Home/index.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+// API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
+
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=> {
+    async function loadMovies(){
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "1f6b33fb81a101dccbcc1b538423c585",
+          page: 1,
+        }
+      })
+
+      console.log(response.data.results);
+
+    }
+
+    loadMovies();
+
+  }, [])
+
   return(
     <div>
       <h1>Welcome to Homepage</h1>
