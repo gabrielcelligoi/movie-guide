687b95cd062d3cf763714e373aa87bf804866c33
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-17 15:48:37 -0300

    create api variable with axios

--- movie-guide/src/services/api.js
+++ movie-guide/src/services/api.js
@@ -1,2 +1,10 @@
+import axios from "axios";
+
 // URL bases: https://api.themoviedb.org/3/
 // API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
