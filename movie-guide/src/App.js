9ebf87d6df4999fb23f5a72d498b3f9b0d2e1627
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-20 17:43:36 -0300

    setting toastify

--- movie-guide/src/App.js
+++ movie-guide/src/App.js
@@ -1,8 +1,14 @@
 import RoutesApp from "./routes";
+import { ToastContainer } from "react-toastify";
+
+// Toastfy stylesheet
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+      {/* configuring the ToastContainer to close in 3s */}
+      <ToastContainer autoClose={3000}/>
       <RoutesApp/>
     </div>
   );
