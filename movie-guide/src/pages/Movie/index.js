c5fc8bcce39a983db62ba28c5acab6cdc234cbfe
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-20 16:44:06 -0300

    create feature to save a movie on localStorage

--- movie-guide/src/pages/Movie/index.js
+++ movie-guide/src/pages/Movie/index.js
@@ -36,6 +36,25 @@ function Movie() {
 
   }, [id]);
 
+  function saveMovie() {
+    // take the list on localstorage and saves it on a variable
+    const myList = localStorage.getItem("@movieguide");
+
+    // take the myList variable, convert it to a string and saves it to a variable named savedMovies. If the myList variable doesnt exist, it saves an empty array to savedMovies variable
+    let savedMovies = JSON.parse(myList) || [];
+
+    // create a variable hasMovie. savedMovies.some will return true if savedMovie.id === movie.id
+    const hasMovie = savedMovies.some((savedMovie) => savedMovie.id === movie.id)
+
+    if(hasMovie) {
+      return;
+    }
+
+    savedMovies.push(movie);
+    localStorage.setItem("@movieguide", JSON.stringify(savedMovies));
+
+  }
+
   if(error) {
     return(
       <Error/>
@@ -60,7 +79,7 @@ function Movie() {
       <strong>Vote Average: {movie.vote_average} / 10</strong>
 
       <div className="area-buttons">
-        <button>Save</button>
+        <button onClick={saveMovie}>Save</button>
         <button>
           {/* target=_blank for open the link in another tab and rel=external to inform broser engines that this is an external link*/}
           <a target="_blank" rel="external" href={`http://youtube.com/results?search_query=${movie.title} Trailer`}>
