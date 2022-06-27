86898c3213ce05ef01fab5da44b407c42393a167
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-27 17:47:40 -0300

    change the main component to show the most popular movies instead of the now_playing movies

--- movie-guide/src/pages/Home/index.js
+++ movie-guide/src/pages/Home/index.js
@@ -11,7 +11,7 @@ function Home() {
 
   useEffect(()=> {
     async function loadMovies(){
-      const response = await api.get("movie/now_playing", {
+      const response = await api.get("movie/popular", {
         params: {
           api_key: "1f6b33fb81a101dccbcc1b538423c585",
           page: 1,
@@ -20,7 +20,7 @@ function Home() {
       
       // console.log(response.data.results.slice(0, 10))
       //using SLICE because the array returned from the API has a lot of movies. So I want to show only the first 10.
-      setMovies(response.data.results.slice(0, 10));      
+      setMovies(response.data.results);      
       setLoading(false);
     }
 
