2ea41c4af7566a17bf24423b4074a63474a40679
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-29 00:20:44 -0300

    refactoring Popular component
    rename css file

--- movie-guide/src/pages/Home/Popular.js
+++ movie-guide/src/pages/Home/Popular.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
-import './home.css';
+import './popular.css';
 
 // API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
 
--- movie-guide/src/pages/Home/index.js
+++ movie-guide/src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
-import './home.css';
 import Popular from "./Popular";
 
 // API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
