60f9750e633196ba3b22701ae8b441d26a0cf286
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-06-29 00:18:40 -0300

    refactoring Popular component

---
+++ movie-guide/src/pages/Home/Popular.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import './home.css';
+
+// API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
+
+function Popular() {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    async function loadMovies(){
+      const response = await api.get("movie/popular", {
+        params: {
+          api_key: "1f6b33fb81a101dccbcc1b538423c585",
+          page: page,
+        }
+      })
+      
+      if (page !== response.data.total_pages && page < 5) {
+        let sumPage = page + 1;
+        setPage(sumPage);
+        
+      }
+      setMovies(prev => [...prev, ...response.data.results]);
+      
+      setLoading(false);
+    }
+
+    loadMovies();
+
+  }, [page])
+
+  if(loading) {
+    return(
+    <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+    )
+  }
+
+  return(
+    <div className="container">
+      <div className="movies-list">
+        {movies.map((movie) => {
+          return(
+            <div key={movie.id}>
+              <Link to={`/movie/${movie.id}`}>
+                <img src={`http://image.tmdb.org//t/p/original/${movie.poster_path}`} alt={movie.title}/>                
+              </Link>
+              <strong>{movie.title}</strong>
+              <strong>{movie.id}</strong>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Popular;
--- movie-guide/src/pages/Home/index.js
+++ movie-guide/src/pages/Home/index.js
@@ -2,61 +2,48 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
 import './home.css';
+import Popular from "./Popular";
 
 // API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
 
 function Home() {
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1)
-  const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState([]);
+  // const [page, setPage] = useState(1)
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(()=> {
-    async function loadMovies(){
-      const response = await api.get("movie/popular", {
-        params: {
-          api_key: "1f6b33fb81a101dccbcc1b538423c585",
-          page: page,
-        }
-      })
+  // useEffect(()=> {
+  //   async function loadMovies(){
+  //     const response = await api.get("movie/popular", {
+  //       params: {
+  //         api_key: "1f6b33fb81a101dccbcc1b538423c585",
+  //         page: page,
+  //       }
+  //     })
       
-      if (page !== response.data.total_pages && page < 5) {
-        let sumPage = page + 1;
-        setPage(sumPage);
+  //     if (page !== response.data.total_pages && page < 5) {
+  //       let sumPage = page + 1;
+  //       setPage(sumPage);
         
-      }
-      setMovies(prev => [...prev, ...response.data.results]);
+  //     }
+  //     setMovies(prev => [...prev, ...response.data.results]);
       
-      setLoading(false);
-    }
+  //     setLoading(false);
+  //   }
 
-    loadMovies();
+  //   loadMovies();
 
-  }, [page])
+  // }, [page])
 
-  if(loading) {
-    return(
-    <div className="loading">
-      <h1>Loading...</h1>
-    </div>
-    )
-  }
+  // if(loading) {
+  //   return(
+  //   <div className="loading">
+  //     <h1>Loading...</h1>
+  //   </div>
+  //   )
+  // }
 
   return(
-    <div className="container">
-      <div className="movies-list">
-        {movies.map((movie) => {
-          return(
-            <div key={movie.id}>
-              <Link to={`/movie/${movie.id}`}>
-                <img src={`http://image.tmdb.org//t/p/original/${movie.poster_path}`} alt={movie.title}/>                
-              </Link>
-              <strong>{movie.title}</strong>
-              <strong>{movie.id}</strong>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <Popular/>
   );
 }
 
