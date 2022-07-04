bd5893a87ae61a109de77dd84e4e6118e2f1f67b
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-04 19:59:25 -0300

    fix the bug that show movies that are already released on Upcoming component

--- movie-guide/src/pages/Home/Upcoming.js
+++ movie-guide/src/pages/Home/Upcoming.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
 import Slider from "react-slick";
-import { BsChevronRight, BsChevronLeft } from "react-icons/fa"
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs"
 
 import './upcoming.css';
 
@@ -11,6 +11,8 @@ import './upcoming.css';
 function Upcoming() {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
+  const [imageIndex, setImageIndex] = useState(0);
+
   const date = new Date();
   const year = date.getFullYear();
   let month = date.getMonth()+1;
@@ -35,10 +37,19 @@ function Upcoming() {
   const NextArrow = ({onClick}) => {
     return (
       <div className="arrow next" onClick={onClick}>
-
+        <BsChevronRight />
       </div>
     )
   }
+
+  const PrevArrow = ({onClick}) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <BsChevronLeft />
+      </div>
+    )
+  }
+  
 
   const settings = {
     infinite:true,
@@ -47,27 +58,33 @@ function Upcoming() {
     slidesToShow: 3,
     centerMode: true,
     centerPadding: 0,
-    nextArrow: <nextArrow/>,
-    prevArrow: <prevArrow/>,
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>,
+    beforeChange: (current, next) => setImageIndex(next),
   }
 
   useEffect(()=> {
     async function loadMovies(){
-      const response = await api.get("movie/upcoming", {
+      await api.get("movie/upcoming", {
         params: {
           api_key: "1f6b33fb81a101dccbcc1b538423c585",
           page: page,
         }
       })
-      
-      //fetch 5 pages of data
-      if (page !== response.data.total_pages && page < 5) {
-        let sumPage = page + 1;
-        setPage(sumPage);        
-      }
+      .then((response) => {
+        
+        let filteredMovies = response.data.results.filter(movie => 
+          movie.release_date >= today && movie.release_date <= releaseLimit && movie.original_language === "en"
+        )
+        setMovies(prev => [...prev, ...filteredMovies])
 
-      setMovies(prev => [...prev, ...response.data.results]);
-      
+        //fetch 5 pages of data
+        if (page !== response.data.total_pages && page < 5) {
+          let sumPage = page + 1;
+          setPage(sumPage);
+        }
+        
+      })
     }
 
     loadMovies();
@@ -81,18 +98,17 @@ function Upcoming() {
     
     <div className="upcoming-list">
       <Slider {...settings}>
-      {movies.map((movie) => {
-        if(movie.release_date >= today && movie.release_date <= releaseLimit && movie.original_language === "en"){
+      {movies.map((movie, idx) => {
           return(
-            <div key={movie.id}>
-              {/* <Link to={`/movie/${movie.id}`}> */}
+            <div key={movie.id} className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+              <Link to={`/movie/${movie.id}`}>
                 <img src={`http://image.tmdb.org//t/p/original/${movie.poster_path}`} alt={movie.title}/>                
-              {/* </Link> */}
+              </Link>
               {/* <strong>{movie.title}</strong>
               <strong>{movie.release_date}</strong> */}
+              <strong>{idx}</strong>
             </div>
           )
-        }
       })}
       </Slider>
     </div>
