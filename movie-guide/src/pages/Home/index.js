7fee1f7ff2b31fc1fb9c6bcf6d014b545397fc4e
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-04 13:45:31 -0300

    fix bug with the alignment of content in homepage

--- movie-guide/src/pages/Home/index.js
+++ movie-guide/src/pages/Home/index.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import api from "../../services/api";
 import Popular from "./Popular";
 import Upcoming from "./Upcoming";
+import './index.css'
 
 // API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
 
 function Home() {
 
   return(
-    <div>
+    <div className="home-container">
       <Popular />
       <Upcoming />
     </div>
