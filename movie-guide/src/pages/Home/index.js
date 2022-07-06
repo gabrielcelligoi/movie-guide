a79fd7511919dbcef6a79e56edab0dcf4f64b33a
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-06 15:11:45 -0300

    create a Banner component to homepage

--- movie-guide/src/pages/Home/index.js
+++ movie-guide/src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import api from "../../services/api";
 import Popular from "./Popular";
 import Upcoming from "./Upcoming";
+import Banner from "./Banner";
 import './index.css'
 
 // API URL: movie/now_playing?api_key=1f6b33fb81a101dccbcc1b538423c585
@@ -11,6 +12,7 @@ function Home() {
 
   return(
     <div className="home-container">
+      <Banner />
       <Popular />
       <Upcoming />
     </div>
