8129ce08a989ac84d42a19e8ce778a022ab1643a
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-06 18:54:41 -0300

    randomize the image on the banner

--- movie-guide/src/pages/Home/Banner.js
+++ movie-guide/src/pages/Home/Banner.js
@@ -1,8 +1,52 @@
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
 import './banner.css'
 
 function Banner() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=> {
+    async function loadMovies(){
+      const response = await api.get(`movie/popular`, {
+        params: {
+          api_key: "1f6b33fb81a101dccbcc1b538423c585",
+          page: 1,
+        }
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+    }
+    
+    loadMovies();
+    
+  }, [])
+  
+  const images = [];
+
+  movies.map((movie) => {
+    images.push(movie.backdrop_path)
+  })
+
+
+  
+  if(images.length !== 0){
+    let randomChange = document.getElementById('randomChange')
+    const imgCount = images.length;
+    let number = Math.floor(Math.random() * imgCount)
+    
+    function imgDisp(num) {
+      randomChange.style.backgroundImage = `linear-gradient(to right, rgba(13, 28, 59, 0.7), rgb(66, 74, 184, 0.7)), url(http://image.tmdb.org/t/p/original/${images[number]})`
+    }
+    console.log(randomChange)
+    console.log(images[number])
+
+    imgDisp()
+  }
+  
+  // `http://image.tmdb.org/t/p/original/${images[number]}`
   return(
-    <div className='banner-container'>
+    <div className='banner-container'  id='randomChange'>
 
     </div>
   )
