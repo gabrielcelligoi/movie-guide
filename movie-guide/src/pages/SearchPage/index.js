74e4e56b92098b7bd8c47c716a1acdb416a89b47
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-08 17:22:44 -0300

    Make request to fetch movies based on a search

--- movie-guide/src/pages/SearchPage/index.js
+++ movie-guide/src/pages/SearchPage/index.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 function Searchpage() {
   const { searchstring } = useParams();
-  console.log(searchstring)
+  const [ searched, setSearched ] = useState([]);
+
+  useEffect(() => {
+    async function loadSearch(){
+      const response = await api.get('search/multi', {
+        params: {
+          api_key: "1f6b33fb81a101dccbcc1b538423c585",
+          query: searchstring,
+        }
+      })
+      setSearched(response.data.results);
+    }
+    loadSearch();
+  }, [])
+
   return(
     <div>
       <h1>{searchstring}</h1>
+      <div className="movie-list">
+        {searched.map(movie => {
+          return(
+            <div key={movie.id}>
+                <Link to={`/movie/${movie.id}`}>
+                  <img src={`http://image.tmdb.org//t/p/original/${movie.poster_path}`} alt={movie.title}/>                
+                </Link>               
+                <strong>{movie.title ? movie.title : movie.name}</strong>              
+              </div>
+          )
+        })}
+      </div>
       
     </div>
   )
