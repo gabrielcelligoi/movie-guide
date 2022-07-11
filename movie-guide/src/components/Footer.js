d19dc5c5940bb44953b5b4189088079df978d44a
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-11 13:33:23 -0300

    fix footer colors

--- movie-guide/src/components/Footer.js
+++ movie-guide/src/components/Footer.js
@@ -1,9 +1,12 @@
 import './footer.css'
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
 
 function Footer() {
   return(
     <footer>
       <p>Created by Gabriel Celligoi</p>
+      <a href='https://github.com/gabrielcelligoi'><AiFillGithub /></a>
+      <a href='https://www.linkedin.com/in/gabrielcelligoi/'><AiFillLinkedin /></a>
     </footer>
   )
 }
